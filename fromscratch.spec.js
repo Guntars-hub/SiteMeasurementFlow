ed9d0376409e5cfa7d422ac4fc7d7288b94ac9e2
Guntars-hub <guntars.grants@gmail.com>
2025-09-14 00:27:30 +0300

    Test

---
+++ fromscratch.spec.js
@@ -0,0 +1,52 @@
+//@ts-check
+
+import { test, expect } from '@playwright/test';
+
+
+
+// Site measuremnt flow
+test('Site measurement flow', async ({ page }) => {
+
+    //Set test timeout 60 seconds
+    test.setTimeout(60000);
+
+    //Open webpage
+    await page.goto('https://dev.explorer.dawninternet.net/');
+
+    //Press Sign up button
+    await page.getByRole('button',{name: 'Sign Up'}).click();
+
+    //Enter email
+    await page.getByPlaceholder('your@email.com').fill('gggunja@gmail.com');
+
+    //Submit entered email
+    await page.getByText('Submit').click();
+
+    //After pasting verification code, press Continue
+    await page.getByRole('button',{name: 'Continue'}).click();
+
+    //Press Site measurement
+    await page.getByText('Site Measurement').click();
+
+    //Press Search Location
+    await page.getByText('Search location').click()
+
+    //Search for Riga
+    await page.getByPlaceholder('Try adresses').fill('Riga');
+
+    //Press search
+    await page.getByLabel('Search location').click();
+    await page.waitForTimeout(1000);
+
+    //Choose first Go button, all buttons identical
+    await page.getByRole('button',{name: 'Go'}).nth(0).click();
+
+    //Press confirm to start measuring
+    await page.getByRole('button', {name: 'Confirm'}).click();
+
+    //Give 120 seconds to load
+    await page.waitForTimeout(20000);
+    
+    //Close browser
+    await page.close();
+})
